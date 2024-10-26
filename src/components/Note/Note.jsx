bb179e40f8f45cf74332b5613fa19041ed0a7697
sinemgrbz <sinemcobanligurbuz@gmail.com>
2024-10-26 15:07:16 +0300

    fixed note component

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -5,7 +5,7 @@ export default function Note ({noteText,color,onDelete}) {
     return (
         <>
                 <div className="note" style={{ backgroundColor: color }}>   
-                    <>{noteText}</p>
+                    <p>{noteText}</p>
                     <i class="fa-regular fa-trash-can" onClick={onDelete}></i>
                 </div>
         </>
